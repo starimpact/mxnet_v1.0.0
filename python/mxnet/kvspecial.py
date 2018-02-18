963717256560c27572f0af1e6e6ee83bf8edbac6
starimpact <mingzhang@deepglint.com>
2018-02-18 19:56:23 +0800

    fill 3 functions in kvspecial.py

--- python/mxnet/kvspecial.py
+++ python/mxnet/kvspecial.py
@@ -20,4 +20,44 @@ class KVSpecial(object):
     pass
   
   def __call__(self, key, inlist, out, kvtype):
+    if 'concat' in kvtype:
+      concat(key, inlist, out)
+    elif 'sum' in kvtype:
+      sum(key, inlist, out)
+    elif 'max' in kvtype:
+      max(key, inlist, out)
+    else:
+      print 'unknown kvtype:', kvtype
+    pass
+
+  def concat(self, key, inlist, out):
+    shape_0 = inlist[0].shape
+    in_num = len(inlist)
+    shape_out = out.shape
+    assert in_num*shape_0[0]==shape_out[0]
+    assert shape_0[1]==shape_out[1]
+    for i in xrange(in_num):
+      out[i*shape_0[0]:(i+1)*shape_0[0]] = inlist[i]
+    pass
+
+  def sum(self, key, inlist, out):
+    shape_0 = inlist[0].shape
+    in_num = len(inlist)
+    shape_out = out.shape
+    assert shape_0==shape_out
+    assert shape_0[1]==shape_out[1]
+    out[:] = inlist[0]
+    for i in xrange(1, in_num):
+      out += inlist[i]
+    pass
+
+  def max(self, key, inlist, out):
+    shape_0 = inlist[0].shape
+    in_num = len(inlist)
+    shape_out = out.shape
+    assert shape_0==shape_out
+    assert shape_0[1]==shape_out[1]
+    out[:] = inlist[0]
+    for i in xrange(1, in_num):
+      out[:] = mx.nd.maximum(out, inlist[1])
     pass
